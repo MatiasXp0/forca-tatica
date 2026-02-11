a7bb99a41565e463586bbea7e6b1c8726438888e
MatiasXp0 <coyotegamer54@gmail.com>
2026-02-11 10:02:19 -0300

    Revert "Update Comunicados.jsx"
    This reverts commit 4c712c88ed64abfd2d3dbc6e9ca45dfd24959578.

--- src/components/Comunicados.jsx
+++ src/components/Comunicados.jsx
@@ -760,7 +760,7 @@ const Comunicados = ({ isAdmin }) => {
                             🔗 LINK DIRETO PARA ESTE COMUNICADO
                           </span>
                           <span className="text-xs text-gray-400 font-mono break-all">
-                            https://forca-tatica.vercel.app/
+                            https://forca-tatica.vercel.app/comunicados?id=
                             {com.id}
                           </span>
                         </div>
